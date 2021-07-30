efd3bf32e515a4601926a98c6318e9912e6cc546
tzec <cesar@elixircreativo.com>
2021-07-29 20:53:41 -0500

    Pluggin
    Pluggin Addon for Stremio

---
+++ addon.js
@@ -0,0 +1,112 @@
+const { addonBuilder } = require("stremio-addon-sdk");
+const magnet = require("magnet-uri");
+
+const manifest = { 
+    "id": "org.elixirtv",
+    "version": "1.0.0",
+
+    "name": "Elixir TV",
+    "description": "Peliculas en idioma latino",
+
+    // set what type of resources we will return
+    "resources": [
+        "catalog",
+        "stream"
+    ],
+
+    "types": ["movie", "series"], // your add-on will be preferred for those content types
+
+    // set catalogs, we'll be making 2 catalogs in this case, 1 for movies and 1 for series
+    "catalogs": [
+        {
+            type: 'movie',
+            id: 'elixirmovies'
+        },
+        {
+            type: 'series',
+            id: 'elixirdseries'
+        }
+    ],
+
+    // prefix of item IDs (ie: "tt0032138")
+    "idPrefixes": [ "tt" ]
+
+};
+
+const dataset = {
+	
+	
+	 // night of the living dead, example from magnet
+    "tt3554046": fromMagnet("Space Jam: A New Legacy", "movie", "magnet:?xt=urn:btih:T7BB5T5NPD7JJS6Q5OT2NKES4LZTUK3Z&dn=Space%20Jam%20-%20Nuevas%20Leyendas%20(2021)%20WEBDL%201080p%20LAT%20-%20CharlX%20%5BWEBRip%20Ligero%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"),
+	
+	
+    // fileIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
+   // "tt0032138": { name: "The Wizard of Oz", type: "movie", infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", fileIdx: 0 },
+    //"tt0017136": { name: "Metropolis", type: "movie", infoHash: "dca926c0328bb54d209d82dc8a2f391617b47d7a", fileIdx: 1 },
+
+    // night of the living dead, example from magnet
+  //  "tt0063350": fromMagnet("Night of the Living Dead", "movie", "magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"),
+    
+   // "tt0051744": { name: "House on Haunted Hill", type: "movie", infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" },
+
+   // "tt1254207": { name: "Big Buck Bunny", type: "movie", url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" }, // HTTP stream
+  //  "tt0031051": { name: "The Arizona Kid", type: "movie", ytId: "m3BKVSpP80s" }, // YouTube stream
+
+   // "tt0137523": { name: "Fight Club", type: "movie", externalUrl: "https://www.netflix.com/watch/26004747" }, // redirects to Netflix
+
+    //"tt1748166:1:1": { name: "Pioneer One", type: "series", infoHash: "07a9de9750158471c3302e4e95edb1107f980fa6" }, // torrent for season 1, episode 1
+};
+
+// utility function to add from magnet
+function fromMagnet(name, type, uri) {
+    const parsed = magnet.decode(uri);
+    const infoHash = parsed.infoHash.toLowerCase();
+    const tags = [];
+    if (uri.match(/720p/i)) tags.push("720p");
+    if (uri.match(/1080p/i)) tags.push("1080p");
+    return {
+        name: name,
+        type: type,
+        infoHash: infoHash,
+        sources: (parsed.announce || []).map(function(x) { return "tracker:"+x }).concat(["dht:"+infoHash]),
+        tag: tags,
+        title: tags[0], // show quality in the UI
+    }
+}
+
+const builder = new addonBuilder(manifest);
+
+// Streams handler
+builder.defineStreamHandler(function(args) {
+    if (dataset[args.id]) {
+        return Promise.resolve({ streams: [dataset[args.id]] });
+    } else {
+        return Promise.resolve({ streams: [] });
+    }
+})
+
+const METAHUB_URL = "https://images.metahub.space"
+
+const generateMetaPreview = function(value, key) {
+    // To provide basic meta for our movies for the catalog
+    // we'll fetch the poster from Stremio's MetaHub
+    // see https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/meta.md#meta-preview-object
+    const imdbId = key.split(":")[0]
+    return {
+        id: imdbId,
+        type: value.type,
+        name: value.name,
+        poster: METAHUB_URL+"/poster/medium/"+imdbId+"/img",
+    }
+}
+
+builder.defineCatalogHandler(function(args, cb) {
+    // filter the dataset object and only take the requested type
+    const metas = Object.entries(dataset)
+	.filter(([_, value]) => value.type === args.type)
+	.map(([key, value]) => generateMetaPreview(value, key))
+
+    return Promise.resolve({ metas: metas })
+})
+
+module.exports = builder.getInterface()
