db1828f1f6016502806a5564ee28d1c1e87a95b1
tzec <cesar@elixircreativo.com>
2023-03-05 13:51:47 -0600

    Update addon.js

--- addon.js
+++ addon.js
@@ -6,7 +6,7 @@ const manifest = {
     "version": "1.0.0",
 
     "name": "Elixir TV",
-    "description": "Peliculas en idioma latino",
+    "description": "Peliculas en español latino",
 
     // set what type of resources we will return
     "resources": [
@@ -24,7 +24,7 @@ const manifest = {
         },
         {
             type: 'series',
-            id: 'elixirdseries'
+            id: 'elixirseries'
         }
     ],
 
@@ -35,9 +35,118 @@ const manifest = {
 
 const dataset = {
 	
+	 // el hombre araña no way home 
+	//   "tt0133093": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:42PWY4QK2JJEEWXTL45ICVSLUWNYXJFC&dn=The%20Matrix%20(1999)%20FullHD&tr=http%3A%2F%2Ftracker.alt-torrent.com%2Fannounce.php"),
 	
-	 // night of the living dead, example from magnet
-    "tt3554046": fromMagnet("Space Jam: A New Legacy", "movie", "magnet:?xt=urn:btih:T7BB5T5NPD7JJS6Q5OT2NKES4LZTUK3Z&dn=Space%20Jam%20-%20Nuevas%20Leyendas%20(2021)%20WEBDL%201080p%20LAT%20-%20CharlX%20%5BWEBRip%20Ligero%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"),
+	// gato con botas
+	   "tt3915174": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:VT3VWNOPHZOIMLEJIMK4JV5A7WY4OSR7&dn=Gato%20con%20Botas%20-%20El%20ultimo%20deseo%20(2022)%20WEBDL%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	
+	// minions 2
+	   "tt5113044": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:SJUFZCO7WOJDV2YNFTKKFKKW5LQWNAPB&dn=Minions%20nace%20un%20villano%20(2022)%20WEBDL%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+
+	
+		// lighyear
+	   "tt10298810": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:5DC6M6WLBXRRS5U7SZBLKVJXTHBDH4PS&dn=Lightyear%20(2022)%20WEBDL%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	// elpeso del talento
+	   "tt11291274": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:APKGWPSR7FJ2R246NC4AFUYPN5UJLK4H&dn=El%20Peso%20del%20Talento%20(2022)%20WEBDL%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	   
+	
+		 // ciudad perdida
+	   "tt13320622": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:AKL3QVOVOTUADRIZ2HKBGHCKRUYY4VZY&dn=La%20ciudad%20perdida%20(2022)%20WEBDL%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	
+			 // matrix 1 br
+	   "tt0133093": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:NCCXKYQZDEXE2WKXDAJHCOOZ3HYMRGMT&dn=The.Matrix.(1999)&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"),
+	
+	
+
+	
+			 // matrix 1 
+	   "tt0133093": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:42PWY4QK2JJEEWXTL45ICVSLUWNYXJFC&dn=The%20Matrix%20(1999)%20FullHD&tr=http%3A%2F%2Ftracker.alt-torrent.com%2Fannounce.php"),
+	
+	
+			 // matrix 2 
+	   "tt0234215": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:OTK2RB2OVFKL772AOU3BZRG2RO72CAEZ&dn=The%20Matrix%20Reloaded%20(2003)%20FullHD&tr=http%3A%2F%2Ftracker.alt-torrent.com%2Fannounce.php"),
+	
+	
+	
+			 // matrix 3
+	   "tt0242653": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:PVAQVWWT62KCJM6SDZV6QAOFUF3SH72C&dn=The%20Matrix%20Revolutions%20(2003)%20FullHD%201080p&tr=http%3A%2F%2Ftracker.alt-torrent.com%2Fannounce.php"),
+	
+	
+	
+		 // matrix 4 
+	   "tt10838180": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:DLKDEGHUN24LV33HT54KVGJWP4YTVF2H&dn=Matrix%20Resurrections%20(2021)%20WEBDL%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	
+	
+	
+		 // escape room 2 
+	   "tt9844522": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:D5QQFQCJXGLFETTTNJ7MTF6PLGTRU5J4&dn=Escape%20Room%202%20-%20Mueres%20por%20salir%20(2021)%20BR-RIP%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	
+	 // free guy  
+	   "tt6264654": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:3423DENKCF3YMIBPSJER63RKMMRCSZA7&dn=Free%20Guy%20(2021)%20BR-RIP%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+
+	
+	
+	 // el mesero
+     "tt10886670": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:5G57OW3WGXAI3DW2C6OQ23CQC3H7D5CF&dn=El%20mesero%20(2021)%20WEBDL%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+
+
+	
+	//sapos
+	    "tt0806938": { name: "Elixir TV", type: "movie", url: "https://lxr.mx/cansada_de_los_sapos.mkv" }, // HTTP stream
+	
+		      // un rescate
+     "tt8049994": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:QHLFYNYAO6MEVMH2RNBCB2SNV76WX576&dn=Un%20Rescate%20de%20Huevitos%20(2021)%20WEBDL%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	
+	      // paw àtrol
+     "tt11832046": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:7UUNVSZPQAE2LZ6O5MQ5C5ZTLA3RUYMJ&dn=La%20patrulla%20canina%20-%20La%20pelicula%20(2021)%20BR-RIP%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	
+	      // sapos
+    // "tt0806938": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:NVEC7VAT5WC5YLIIQWOA36NOFCZJKXYP&dn=c4ns4d4.d3.b3s4r.s4p0s.dvdrip.mp4&xl=447048338&tr=http%3A%2F%2Fopentracker.i2p.rocks%3A6969%2Fannounce"),
+	
+	       // misfits
+     "tt4876134": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:QRCNIXEXEPQ6BXP3BNF7ASVN43GNSR6B&dn=Ladrones%20de%20Elite%20(2021)%20WEBDL%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	
+		 // escuadron suicida
+     "tt6334354": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:O4OYJNKKLQH3EF6ATL3EMAQ5SZ5ZMMH7&dn=El%20Escuadron%20Suicida%20(2021)%20WEBDL%201080p%20LAT%20-%20CharlX%20%5BWEBRip%20Ligero%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"),
+     
+	
+	//Peter Rabbit 2
+	"tt8376234": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:X3RGJUXLWSJKFIDO7LO64XGRGMNJWWOG&dn=Peter%20Rabbit%202%20-%20A%20la%20fuga%20(2021)%20BR-RIP%201080p%20LAT%20-%20CharlX&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"),
+	
+	
+	//Midnight in the Switchgrass
+	"tt11656220": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:QFPE6GS5IATYRDT2DZNBSB6CSR57E3ZC&dn=Midnight%20in%20the%20Switchgrass%20(2021)%20FullHD%201080p%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	// black widow
+     "tt3480822": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:WDHBK6UYDPXNPAAARUZLQGVPZWRDAMVZ&dn=Viuda%20Negra%20(2021)%20WEBDL-DSNY%201080p%20LAT%20-%20CharlX%20%5BWEBRip%20Ligero%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"),
+
+	// fast and furious 9
+     "tt5433138": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:SIMIYJ5KQVATEGEMKJEYQWM2IJ7ANXPC&dn=Fast%20%20Furious%209%20(2021)%20WEBDL-AMZN%201080p%20LAT%20-%20CharlX%20%5BWEBRip%20Ligero%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"),
+
+	
+	// jefe en pañales 2
+     "tt6932874": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:PK64IANW2TNC3VDXALF6XIAJPLDNN65Y&dn=El%20bebe%20jefazo%20-%20Negocios%20de%20familia%20(2021)%20WEBDL%201080p%20H.265%20LAT%20-%20ZeiZ&tr=http%3A%2F%2Fexplodie.org%3A6969%2Fannounce"),
+	
+	 // magnet space jam
+    "tt3554046": fromMagnet("Elixir-TV", "movie", "magnet:?xt=urn:btih:T7BB5T5NPD7JJS6Q5OT2NKES4LZTUK3Z&dn=Space%20Jam%20-%20Nuevas%20Leyendas%20(2021)%20WEBDL%201080p%20LAT%20-%20CharlX%20%5BWEBRip%20Ligero%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"),
+    
+    	 // magnet jungle cruise
+     "tt0870154": fromMagnet("Elixir TV", "movie", "magnet:?xt=urn:btih:KJAPRNV3O2JBFJFCZCXZAIB7F6IA2YRO&dn=Jungle%20Cruise%20(2021)%20WEBDL-DSNY%201080p%20LAT%20-%20CharlX%20%5BWEBRip%20Ligero%5D&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337%2Fannounce"),
+     
+         	 
+
+ //   "tt0460091:1:1": { name: "Elixir-TV", type: "series", "url": "https://drive.google.com/file/d/1p_ZJLx8iEfOVGu5pDAK948sX3Qwtzhfu/view?usp=sharing" }, // torrent for season 1, episode 1
+   
 	
 	
     // fileIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
